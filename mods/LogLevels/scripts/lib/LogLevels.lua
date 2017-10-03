dae8ee67e133e44f2c3a8cb64ba947d333bc1116
kalvindukes <4587528+kalvindukes@users.noreply.github.com>
2017-10-03 14:21:20 +0200

    .gitignore -> no more .zip logLevels -> added reverse lookups for levels PrintLog -> removed for loops; taken reverse lookups instead

--- mods/LogLevels/scripts/lib/LogLevels.lua
+++ mods/LogLevels/scripts/lib/LogLevels.lua
@@ -1,2 +1,3 @@
-local logLevels = {off = 0, fatal = 100, error = 200, warning = 300, info = 400, debug = 500, trace = 600, all = 999}
+local logLevels = {	off = 0, fatal = 100, error = 200, warning = 300, info = 400, debug = 500, trace = 600, all = 999,
+					[0] = 'off', [100] = 'fatal', [200] = 'error', [300] = 'warning', [400] = 'info', [500] = 'debug', [600] = 'trace', [999] = 'all'}
 return logLevels
--- mods/LogLevels/scripts/lib/PrintLog.lua
+++ mods/LogLevels/scripts/lib/PrintLog.lua
@@ -1,61 +1,72 @@
 package.path = package.path .. ";mods/LogLevels/scripts/lib/?.lua"
 local levels = require('LogLevels')
 local oldprint = print
+local prepend
 
 -- override the default print function
 print = function(...)
-  if Server == nil then
+    if Server == nil then
       oldprint(...)
-  else
-    --Get server value set via cmd or server.lua
-    local InfoLevel = levels.info or 400
-    local ServerSetting = Server():getValue('log_level') or InfoLevel
-    local LogCurrentLevel = tonumber(ServerSetting)
+    else
+        --Get server value set via cmd or server.lua
+        local InfoLevel = levels.info or 400
+        local ServerSetting = Server():getValue('log_level') or InfoLevel
+        local LogCurrentLevel = tonumber(ServerSetting)
 
-    local ConsoleServerSetting = Server():getValue('console_level') or InfoLevel
-    local ConsoleCurrentLevel = tonumber(ConsoleServerSetting)
+        local ConsoleServerSetting = Server():getValue('console_level') or InfoLevel
+        local ConsoleCurrentLevel = tonumber(ConsoleServerSetting)
 
-    --Assume level info
-    local PrintLevel = InfoLevel
-    local args = table.pack(...)
-    local hadPrintLevel = false
-    --Set the level at which the print is attempting to print at.
-    if #args > 1 and args[#args] ~= nil then
-      --cast last argument to number
-      local tempArg = tonumber(args[#args])
-      --if argument is a number, set the printLevel
-      if type(tempArg) == "number" then
-        --if the number matches one of our loglevels
-        for _, logLevel in pairs(levels) do
+        -- early return on *level mismatch
 
-          if tempArg == logLevel then
-            PrintLevel = tempArg
-            hadPrintLevel = true
-            break
-          end
+        -- gracefully handle tables
+
+
+        --Assume level info
+        local printLevel = InfoLevel
+        local args = table.pack(...)
+        local hadPrintLevel = false
+        prepend = '['..levels[printLevel]..'] '
+        --Set the level at which the print is attempting to print at.
+        if #args > 1 and args[#args] ~= nil then
+            --cast last argument to number
+            local tempArg = tonumber(args[#args])
+            --if argument is a number, set the printLevel
+            if type(tempArg) == "number" and tempArg < 1000 and tempArg >=0 then
+                --at this point we're sure the new structure of print() is used, assuming last number is an actual, desired loglevel
+                printLevel = tempArg
+
+                local printText = levels[printLevel]
+                if printText ~= nil then
+                    hadPrintLevel = true
+                    prepend = '['..string.upper(printText)..']  '
+                else
+                    --prepend messages
+                    prepend = '['..printLevel..'] '
+                end
+
+                -- --if the number matches one of our loglevels,
+                -- for index, logLevel in pairs(levels) do
+                --
+                --     if tempArg == logLevel then
+                --         printLevel = tempArg
+                --         hadPrintLevel = true
+                --         prepend = '['..string.upper(index)..']  '
+                --         break
+                --     end
+                -- end
+                --if we had a log level remove it so its not printed
+                if hadPrintLevel then args[#args] = nil end
+
+            else
+                --assuming vanilla print call
+            end
+
         end
-      end
+        --if we're printing this message
+        if printLevel <= ConsoleCurrentLevel then
+          oldprint(prepend..table.unpack(args))
+        elseif printLevel <= LogCurrentLevel then
+          printlog(prepend..table.unpack(args))
+        end
     end
-
-    --prepend messages
-    local prepend = ''
-    for index,value in pairs(levels) do
-      if PrintLevel == value then
-        prepend = '['..string.upper(index)..']  '
-        break
-      end
-    end
-
-    --add prepend to begging of message
-    table.insert(args,1,prepend)
-    --if we had a log level remove it so its not printed
-    if hadPrintLevel then args[#args] = nil end
-
-    --if were printing this message
-    if PrintLevel <= ConsoleServerSetting then
-      oldprint(table.unpack(args))
-    elseif PrintLevel <= LogCurrentLevel then
-      printlog(table.unpack(args))
-    end
-  end
 end
